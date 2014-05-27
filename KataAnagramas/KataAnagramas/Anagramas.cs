b0c0395f1c6f788156d2a585a42a3fd848f32d89
ssabato <sebastian.sabato@cpba.com.ar>
2014-05-27 09:17:04 -0300

    Primer test en verde. Comienzo implementando el codigo minimo que haga que el test pase.

---
+++ KataAnagramas/KataAnagramas/Anagramas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataAnagramas
+{
+    public class Anagramas
+    {
+        public static List<string> obtenerAnagramas(string strPalabraOriginal)
+        {
+            List<String> resultados = new List<string>();
+            resultados.Add("Orma");
+            resultados.Add("Amor");
+            resultados.Add("Omar");
+            resultados.Add("Mora");
+            resultados.Add("Ramo");
+            resultados.Add("Armo");
+            return resultados;
+        }
+    }
+}
---
+++ KataAnagramas/UnitTestAnagramas/UnitTestAnagramas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KataAnagramas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestAnagramas
+{
+    [TestClass]
+    public class UnitTestAnagramas
+    {
+        [TestMethod]
+        public void TestMethodAnagramasRomaDevuelmeAmorOmarRamoOrmaMoraArmo()
+        {
+            List<String> resultadosEsperados = new List<String>();
+            List<String> resultadosObtenidos = new List<String>();
+            resultadosEsperados.Add("Amor");
+            resultadosEsperados.Add("Omar");
+            resultadosEsperados.Add("Ramo");
+            resultadosEsperados.Add("Orma");
+            resultadosEsperados.Add("Mora");
+            resultadosEsperados.Add("Armo");
+            resultadosObtenidos = Anagramas.obtenerAnagramas("Roma");
+            resultadosEsperados.Sort();
+            resultadosObtenidos.Sort();
+            Assert.IsTrue((resultadosEsperados.SequenceEqual(resultadosObtenidos)),"");
+        }
+    }
+}
